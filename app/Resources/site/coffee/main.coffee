1ca585347a7384e68b5ea447c485505a2659936b
AveVlad <dev@vld.me>
2014-08-15 13:22:57 +0400

    add site theme

---
+++ app/Resources/site/coffee/main.coffee
@@ -0,0 +1,4 @@
+confrimDelete = ->
+
+$ ->
+  do confrimDelete
---
+++ src/Maps/PageBundle/Controller/PageController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Maps\PageBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+
+class PageController extends Controller
+{
+    /**
+     * Lists all Page entities.
+     *
+     * @Route("/{slug}-{id}", requirements={"slug" = "[a-zA-Z|-]+", "id" = "\d+"})
+     * @Method("GET")
+     * @Template()
+     */
+    public function showAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $entiti = $em->getRepository('MapsPageBundle:Page')->find([
+            'id' => 12
+        ]);
+
+        if (!$entiti) {
+            throw $this->createNotFoundException('The page does not exist');
+        }
+
+        return [
+            'entiti' => $entiti,
+        ];
+    }
+
+}
