97f7538f35e30e5c0129ee7080b43c72e47d8918
AveVlad <dev@vld.me>
2014-10-12 16:59:56 +0400

    fix menu and search page

--- src/Maps/MainBundle/Menu/MenuBuilder.php
+++ src/Maps/MainBundle/Menu/MenuBuilder.php
@@ -45,8 +45,8 @@ class MenuBuilder extends ContainerAware
     public function homeMenuRight(FactoryInterface $factory)
     {
         $menu = $factory->createItem('root');
-        $menu->addChild('Чат единомышленников', ['uri' => 'http://forum.ednml.ru/chat/'])->setLinkAttributes(['target' => '_blank']);
         $menu->addChild('Форум поиска единомышленников', ['uri' => 'http://forum.ednml.ru/'])->setLinkAttributes(['target' => '_blank']);
+        $menu->addChild('Чат единомышленников', ['uri' => 'http://forum.ednml.ru/chat/'])->setLinkAttributes(['target' => '_blank']);
 
         return $menu;
     }
